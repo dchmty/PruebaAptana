44da06c87d2f7790266e0df0f688cccb3a129f24
David Chavez <dchmty@hotmail.com>
2012-10-03 21:47:54 -0500

    first commit aptana

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PruebaAptana::Application.config.secret_token = 'caffb8a98faf8883c95a978d0c2457122e15a019707a3169418f2a58fb85e771975ede7e32e244d2bb0b18e66da8a3579abd8a2c0bb7acd2f80c43c0d1b287ac'
